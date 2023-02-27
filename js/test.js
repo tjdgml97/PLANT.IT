972727f982419e1c275b347245a0411521c3a202
dbswp <dbswp980427@gmail.com>
2023-02-27 15:20:33 +0900

    feat[#11] : 질문지 기능-> 질문 클릭시 다음질문 생성

--- js/test.js
+++ js/test.js
@@ -1,21 +1,53 @@
 // 스크롤 이동 기능
 // 클래스, 아이디 값을 인자로 받아 offsetTop 밸류 초기화
 // 초기화된 offsetTop 값을 위치로 이동
-const questionBox = document.querySelector(".question-box");
-const answer = questionBox.querySelector(".answer");
-const btn = document.querySelector(".btn");
-const checkbox = document.createElement("input");
 
-function goToScroll(name) {
-  var location = document.querySelector(".question" + name).offsetTop;
+const questionBox = document.querySelector("#question-box");
+const question1 = document.querySelector(".question1");
+const question2 = document.querySelector(".question2");
+const question3 = document.querySelector(".question3");
+const question = questionBox.querySelector("p.question");
+const answer = question.querySelector("p.answer");
+const btn = document.querySelector(".btn");
+const resultBtn = document.querySelector(".result-btn");
+
+function movetoScroll() {
+  const location = questionBox.offsetTop;
+  console.log(location);
   window.scrollTo({ top: location, behavior: "smooth" });
+  question1.classList.remove("hide");
+  question1.style.transform = "translateX(0)";
+  console.log("hi");
 }
 
-function movotoScroll() {}
+function movetoQuestion2(check) {
+  if (check.checked === true) {
+    question2.classList.remove("hide");
+    question2.style.transform = "translateX(0)";
+  }
+}
 
-// window.addEventListener("scroll", function () {
-//   console.log(window.scrollY);
-//   900
-// });
+function movetoQuestion3(check) {
+  if (check.checked === true) {
+    const location2 = document.body.scrollHeight;
+    window.scrollTo({ top: location2, behavior: "smooth" });
+    question3.classList.remove("hide");
+    question3.style.transform = "translateX(0)";
+  }
+}
 
-btn.addEventListener("click", movotoScroll);
+function movetoResult(check) {
+  if (check.checked === true) {
+    question3.classList.remove("hide");
+    question3.style.transform = "translateX(0)";
+    resultBtn.classList.remove("hide");
+  }
+}
+
+btn.addEventListener("click", movetoScroll);
+
+window.onload = function () {
+  setTimeout(function () {
+    scrollTo(0, 0);
+  }, 100);
+};
