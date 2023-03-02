6c178dc1f10930eff537c3a9288cfefc2d6c615c
dbswp <dbswp980427@gmail.com>
2023-03-02 09:12:36 +0900

    rebase 작업

--- js/test.js
+++ js/test.js
@@ -126,3 +126,15 @@ hardQuestion.addEventListener("click", btnHard);
 
 shortQuestion.addEventListener("click", btnShort);
 longQuestion.addEventListener("click", btnLong);
+
+// header
+const menuIcon = document.querySelector(".menu-icon");
+const menuEl = document.querySelector(".menu-bar-menu");
+
+menuIcon.addEventListener("click", function () {
+  if (menuEl.classList.contains("menu-hidden")) {
+    menuEl.classList.remove("menu-hidden");
+  } else {
+    menuEl.classList.add("menu-hidden");
+  }
+});
