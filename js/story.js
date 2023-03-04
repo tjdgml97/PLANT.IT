dbe2a5ce43ee5bb5e72e89f93e5cbc7cf098fcff
dbswp <dbswp980427@gmail.com>
2023-03-04 18:48:33 +0900

    feat[#54] : merit 색션 차트 만드는 중

--- js/story.js
+++ js/story.js
@@ -71,6 +71,8 @@ function moveto1() {
 
 //도넛모양 차트 만들기
 const donut = document.querySelector(".donut");
+const donut1 = document.querySelector(".donut1");
+// const donutbox = document.querySelector(".donut-box");
 
 function moveto2() {
   if (merit2.classList.contains("move1")) {
@@ -79,35 +81,59 @@ function moveto2() {
     merit3.classList.remove("hide");
     merit4.classList.remove("hide");
     donut.classList.add("hide");
+    donut1.classList.add("hide");
+    // donutbox.classList.add("hide");
   } else {
     merit2.classList.add("move1");
     merit1.classList.add("hide");
     merit3.classList.add("hide");
     merit4.classList.add("hide");
+    const totalMinwon = donut.dataset.percent;
+    const totalMinwon1 = donut1.dataset.percent;
+    donut.style.background = `conic-gradient(#3F8BC9 0% ${totalMinwon}%, #F2F2F2 ${totalMinwon}% 100%)`;
+    donut1.style.background = `conic-gradient(#3F8BC9 0% ${totalMinwon1}%, #F2F2F2 ${totalMinwon1}% 100%)`;
+    let t4 = 0;
+    let t5 = 0;
+    const donutAnimation = setInterval(() => {
+      donut.dataset.percent = t4;
+      donut.style.background = `conic-gradient(#4F98FF 0 ${t4}%, #DEDEDE ${t4}% 100% )`;
+      t4++ >= totalMinwon && clearInterval(donutAnimation);
+    }, 10);
+    const donutAnimation1 = setInterval(() => {
+      donut1.dataset.percent = t5;
+      donut1.style.background = `conic-gradient(#4F98FF 0 ${t5}%, #DEDEDE ${t5}% 100% )`;
+      t5++ >= totalMinwon1 && clearInterval(donutAnimation1);
+    }, 10);
     donut.classList.remove("hide");
+    donut1.classList.remove("hide");
+    // donutbox.classList.remove("hide");
   }
 }
+
+// 막대바 만들기
+const progress = document.querySelector(".progress-box");
+const bar = progress.querySelector(".progress");
+
 function moveto3() {
   if (merit3.classList.contains("move1")) {
     merit3.classList.remove("move1");
     merit2.classList.remove("hide");
     merit1.classList.remove("hide");
     merit4.classList.remove("hide");
-    reason.classList.add("hide");
+    progress.classList.add("hide");
   } else {
     merit3.classList.add("move1");
     merit2.classList.add("hide");
     merit1.classList.add("hide");
     merit4.classList.add("hide");
-    const totalMinwon = donut.dataset.percent;
-    donut.style.background = `conic-gradient(#3F8BC9 0% ${totalMinwon}%, #F2F2F2 ${totalMinwon}% 100%)`;
-    let t4 = 0;
-    const donutAnimation = setInterval(() => {
-      donut.dataset.percent = t4;
-      donut.style.background = `conic-gradient(#4F98FF 0 ${t4}%, #DEDEDE ${t4}% 100% )`;
-      t4++ >= totalMinwon && clearInterval(donutAnimation);
+    const totalMinwon = 44;
+    let t = 0;
+    bar.style.width = 0;
+    const barAnimation = setInterval(() => {
+      bar.style.width = t + "%";
+      t++ >= totalMinwon && clearInterval(barAnimation);
     }, 10);
-    reason.classList.remove("hide");
+    progress.classList.remove("hide");
   }
 }
 function moveto4() {
